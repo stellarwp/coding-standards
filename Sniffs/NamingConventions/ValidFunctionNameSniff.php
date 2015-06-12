ba1840599c90d056ca99fea8679ce6f8c6a13f6b
Matthew Batchelder <borkweb@gmail.com>
2015-06-12 08:55:16 -0400

    Don't extend PEAR's function sniff because it does more than we want

--- Sniffs/NamingConventions/ValidFunctionNameSniff.php
+++ Sniffs/NamingConventions/ValidFunctionNameSniff.php
@@ -18,7 +18,7 @@
  * @package  PHP_CodeSniffer
  * @author   John Godley <john@urbangiraffe.com>
  */
-class TribalScents_Sniffs_NamingConventions_ValidFunctionNameSniff extends PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff
+class TribalScents_Sniffs_NamingConventions_ValidFunctionNameSniff extends PHP_CodeSniffer_Standards_AbstractScopeSniff
 {
 
     private $_magicMethods = array(
@@ -39,6 +39,20 @@ class TribalScents_Sniffs_NamingConventions_ValidFunctionNameSniff extends PEAR_
                              );
 
     /**
+     * A list of all PHP magic functions.
+     *
+     * @var array
+     */
+    protected $magicFunctions = array('autoload');
+
+    /**
+     * Constructs a PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff.
+     */
+    public function __construct() {
+        parent::__construct(array(T_CLASS, T_INTERFACE, T_TRAIT), array(T_FUNCTION), true);
+    }//end __construct()
+
+    /**
      * Processes the tokens within the scope.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being processed.
@@ -48,8 +62,7 @@ class TribalScents_Sniffs_NamingConventions_ValidFunctionNameSniff extends PEAR_
      *
      * @return void
      */
-    protected function processTokenWithinScope(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $currScope)
-    {
+    protected function processTokenWithinScope(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $currScope) {
         $className  = $phpcsFile->getDeclarationName($currScope);
         $methodName = $phpcsFile->getDeclarationName($stackPtr);
 
@@ -74,5 +87,41 @@ class TribalScents_Sniffs_NamingConventions_ValidFunctionNameSniff extends PEAR_
             return;
         }
     }//end processTokenWithinScope()
+
+    /**
+     * Processes the tokens outside the scope.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being processed.
+     * @param int                  $stackPtr  The position where this token was
+     *                                        found.
+     *
+     * @return void
+     */
+    protected function processTokenOutsideScope(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $functionName = $phpcsFile->getDeclarationName($stackPtr);
+        if ($functionName === null) {
+            // Ignore closures.
+            return;
+        }
+
+        if (ltrim($functionName, '_') === '') {
+            // Ignore special functions.
+            return;
+        }
+
+        $errorData = array($functionName);
+
+        // Is this a magic function. i.e., it is prefixed with "__".
+        if (preg_match('|^__|', $functionName) !== 0) {
+            $magicPart = strtolower(substr($functionName, 2));
+            if (in_array($magicPart, $this->magicFunctions) === false) {
+                 $error = 'Function name "%s" is invalid; only PHP magic methods should be prefixed with a double underscore';
+                 $phpcsFile->addError($error, $stackPtr, 'FunctionDoubleUnderscore', $errorData);
+            }
+
+            return;
+        }
+    }//end processTokenOutsideScope()
 }//end class
 
